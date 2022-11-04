23b8456c3f512853b2756ab7958a6b254551ce0a
H4M5TER <zhuxx2003@gmail.com>
2022-11-05 03:37:06 +0800

    sort extracted

--- extract_name.js
+++ extract_name.js
@@ -17,7 +17,7 @@ try {
     const content = fs.readFileSync(`game/${file}.py`, 'utf-8').trim()
     const lines = content.split(/\r?\n/)
     const matched = lines.filter(line => regex.test(line)).map(line => line.trim())
-    const result = matched.join('\n')
+    const result = matched.sort().join('\n')
     fs.writeFileSync(`generated/${file}.py`, result,'utf-8')
   }
 } catch (error) {
