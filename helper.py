06855122a4a73aaba7b8a76b27fa81957653be59
H4M5TER <h4m5ter@outlook.com>
2024-04-25 03:24:41 +0800

    fix order

--- helper.py
+++ helper.py
@@ -4,10 +4,13 @@ import Spells, Upgrades, Consumables, Equipment, Monsters, RareMonsters
 from Equipment import RandomWand, RandomSheild, RandomLittleRing
 from functools import reduce
 
+spells = Spells.make_player_spells()
+skills = Upgrades.make_player_skills()
+
 tasks = [
-    ("spells", [c().name for c in Spells.all_player_spell_constructors]),
-    ("skills", [c().name for c in Upgrades.skill_constructors]),
-    ('upgrades', [u.name for u in reduce(lambda a, b: a + b, [c().spell_upgrades for c in Spells.all_player_spell_constructors])]),
+    ("spells", [spell.name for spell in spells]),
+    ("skills", [skill.name for skill in skills]),
+    ('upgrades', sorted([u.name for u in reduce(lambda a, b: a + b, [spell.spell_upgrades for spell in spells])])),
     ("consumables", [c().name for (c, _) in Consumables.all_consumables]),
     ("equipments", [c().name for c in Equipment.all_items if c not in [RandomWand, RandomSheild, RandomLittleRing]]),
     ("monsters", [c().name for (c, _) in Monsters.spawn_options]),
@@ -18,7 +21,7 @@ def process(names):
     return "".join([f"\n    \"{n}\": \"{n}\"," for n in names])
 
 # jsons = [f"{type} = {{{"".join([f"\n\t\"{n}\" = \"{n}\"," for n in names])}\n}}\n" for (type, names) in tasks]
-jsons = [f"{type} = {{{process(sorted(names))}\n}}\n" for (type, names) in tasks]
+jsons = [f"{type} = {{{process(names)}\n}}\n" for (type, names) in tasks]
 
 f = open("extracted.py", "w")
 f.write('\n'.join(jsons))
